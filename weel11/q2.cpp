703a6746b8dcd68f7837cc4f6939a3537d590ef0
Gurmeet-Rana <ranagur361@gmail.com>
2025-05-22 08:47:04 +0530

    Coin Change

--- weel11/q2.cpp
+++ weel11/q2.cpp
@@ -1,3 +1,34 @@
 /* Given a set of available types of coins. Let suppose you have infinite supply of each type of coin.
 For a given value N, you have to Design an algorithm and implement it using a program to find
 number of ways in which these coins can be added to make sum value equals to N */
+#include<iostream>
+#include<algorithm>
+#include<numeric>
+#include<vector>
+using namespace std;
+int totalWays(vector<int>&coins,int target,int i)
+{
+    if(target==0) return 1;
+    if(i>=coins.size() || target<0) return 0;
+    if(target<coins[i]) return totalWays(coins,target,i+1);
+    return totalWays(coins,target-coins[i],i)+totalWays(coins,target-coins[i],i+1);
+}
+int main()
+{
+    int n;
+    cout<<"Enter the number of coins : "<<endl;
+    cin>>n;
+    vector<int> coins;
+    for(int i=0;i<n;i++)
+    {
+        int coin;
+        cout<<"Enter "<<i<<" th coin "<<endl;
+        cin>>coin;
+        coins.push_back(coin);
+    }
+    int target;
+    cout<<"Enter the target sum : "<<endl;
+    cin>>target;
+    cout<<totalWays(coins,target,0);
+    return 0;
+}
