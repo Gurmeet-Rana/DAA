8e43478ec0676c65c9df14d85b1fa15d8abe0648
Gurmeet-Rana <ranagur361@gmail.com>
2025-07-16 15:48:58 +0530

    Gas Stations problem

--- STACK AND Queue/qUsingSt.cpp
+++ STACK AND Queue/qUsingSt.cpp
@@ -6,7 +6,7 @@ class Queue
     public : 
         stack<int> st1,st2;
         // m1 : enqueue easy 
-        void enqueue(int val)
+        /* void enqueue(int val)
         {
             st1.push(val);
         }
@@ -56,6 +56,59 @@ class Queue
         bool isEmpty()
         {
             return st1.empty();
+        } */
+
+        // METHOD 2 : DEQUEUE EASY 
+        void enqueue(int val)
+        {
+            while(!st1.empty())
+            {
+                st2.push(st1.top());
+                st2.pop();
+            }
+            st2.push(val);
+            while(!st2.empty())
+            {
+                st1.push(st2.top());
+                st2.pop();
+            }
+
+        } 
+        int dequeue()
+        {
+            if(st1.empty())
+            {
+                cout<<"Queue is empty!!"<<endl;
+                return -1;
+            }
+            int val=st1.top();
+            st1.pop();            
+            return val;
+        }
+        bool isEmpty()
+        {
+            return st1.empty();   
+        }
+        int getFront()
+        {
+            if(st1.empty())
+            {
+                cout<<"Queue is empty!!"<<endl;
+                return -1;
+            }
+            int val;
+            while(st1.size()!=1)
+            {
+                st2.push(st1.top());
+                st1.pop();
+            }
+            val=st1.top();
+            while(!st2.empty())
+            {
+                st1.push(st2.top());
+                st2.pop();
+            }
+            return val;
         }
 };
 int main()
@@ -78,7 +131,7 @@ int main()
         }
             
         case 2 :
-            cout<<"Dequeue : "<<q.deque()<<endl; 
+            cout<<"Dequeue : "<<q.dequeue()<<endl; 
             break;
         case 3 : 
             cout<<"Front : "<<q.getFront()<<endl;
