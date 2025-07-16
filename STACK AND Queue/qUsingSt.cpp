12def52294348e554cbb1872148ded93c8177c78
Gurmeet-Rana <ranagur361@gmail.com>
2025-07-16 14:09:36 +0530

    Queue using stack : Method 1 Optimal Enqueue

--- STACK AND Queue/qUsingSt.cpp
+++ STACK AND Queue/qUsingSt.cpp
@@ -12,7 +12,9 @@ class Queue
         }
         int deque()
         {
-            while(!st1.empty())
+            if(!st1.empty())
+            {
+                while(!st1.empty())
             {
                 st2.push(st1.top());
                 st1.pop();
@@ -25,6 +27,31 @@ class Queue
                 st2.pop();
             }
             return val;
+            }
+            else{
+                cout<<"Stack is empty !!"<<endl;
+                return -1;
+            }
+        }
+        int getFront()
+        {
+            if(!st1.empty())
+            {
+                while(st1.size()!=1)
+                {
+                    st2.push(st1.top());
+                    st1.pop();
+                } 
+                int val=st1.top();
+                while(!st2.empty())
+                {
+                    st1.push(st2.top());
+                    st2.pop();
+                }
+                return val;
+            } 
+            cout<<"Stack is empty !!"<<endl;
+            return -1;
         }
         bool isEmpty()
         {
@@ -34,12 +61,37 @@ class Queue
 int main()
 {
     int choice=-1;
+    Queue q ; 
     do
     {
-        cout<<"Press : \n1 "
+        cout<<"Press : \n1 Enqueue \n2 Dequeue \n3 Get Front \n4 Check Empty \n5 Exit "<<endl;
+        cin>>choice;
+        switch (choice)
+        {
+        case 1:
+        {
+            int ele;
+            cout<<"Enter value to enqueue : "<<endl;
+            cin>>ele;
+            q.enqueue(ele);
+            break;
+        }
+            
+        case 2 :
+            cout<<"Dequeue : "<<q.deque()<<endl; 
+            break;
+        case 3 : 
+            cout<<"Front : "<<q.getFront()<<endl;
+            break;
+        case 4 : 
+            cout<<"Empty : "<<q.isEmpty()<<endl;
+            break;
+        default:
+            break;
+        }
         /* code */
-    } while (choice!=4);
+    } while (choice!=5);
      
-    
+    cout<<"EXITING THE PROGRAM..."<<endl;
     return 0;
 }
